86a8544e1c59d8c12828cb22be09c16f645f9b0d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-11-30 22:22:55 +0100

    Use graphql-tag to declare type definitions

--- src/schema.js
+++ src/schema.js
@@ -1,113 +1,48 @@
-import {
-  GraphQLID,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLSchema,
-  GraphQLString,
-  GraphQLNonNull,
-  GraphQLBoolean,
-  GraphQLObjectType,
-} from 'graphql';
+import gql from 'graphql-tag'
 
-import { GraphQLEmailAddress } from './scalars/EmailAddress';
+const typeDefs = gql`
+  scalar EmailAddress
 
-const getNonNullListType = (type) => (
-  new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(type)))
-)
-
-const User = new GraphQLObjectType({
-  name: 'User',
-  description: 'A application user',
-  fields: () => ({
-    id: {
-      type: new GraphQLNonNull(GraphQLID),
-    },
-    age: {
-      type: GraphQLInt
-    },
-    name: {
-      type: new GraphQLNonNull(GraphQLString)
-    },
-    email: {
-      type: new GraphQLNonNull(GraphQLEmailAddress)
-    },
-    posts: {
-      type: getNonNullListType(Post)
-    },
-    comments: {
-      type: getNonNullListType(Comment)
-    }
-  })
-})
-
-const Post = new GraphQLObjectType({
-  name: 'Post',
-  description: "A user's post",
-  fields: () => ({
-    id: {
-      type: new GraphQLNonNull(GraphQLID)
-    },
-    body: {
-      type: new GraphQLNonNull(GraphQLString)
-    },
-    title: {
-      type: new GraphQLNonNull(GraphQLString)
-    },
-    isPublished: {
-      type: new GraphQLNonNull(GraphQLBoolean)
-    },
-    author: {
-      type: new GraphQLNonNull(User)
-    },
-    comments: {
-      type: new GraphQLList(new GraphQLNonNull(Comment))
-    }
-  })
-})
-
-const Comment = new GraphQLObjectType({
-  name: 'Comment',
-  description: "A user's comment",
-  fields: {
-    id: {
-      type: new GraphQLNonNull(GraphQLID)
-    },
-    text: {
-      type: new GraphQLNonNull(GraphQLString)
-    },
-    author: {
-      type: new GraphQLNonNull(User)
-    },
-    post: {
-      type: new GraphQLNonNull(Post)
-    }
+  type Query {
+    me: User! 
+    posts: [Post!]
+    users: [User!]
+    comments: [Comment!] 
   }
-})
 
-const typeDefs = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      me: {
-        type: new GraphQLNonNull(User)
-      },
-      users: {
-        type: getNonNullListType(User),
-        args: {
-          query: GraphQLString
-        }
-      },
-      posts: {
-        type: getNonNullListType(Post),
-        args: {
-          query: GraphQLString
-        }
-      },
-      comments: {
-        type: getNonNullListType(Comment)
-      }
-    }
-  })
-})
+  """
+  A type that describes the user. 
+  """
+  type User {
+    id: ID!
+    age: Int
+    name: String! 
+    posts: [Post!]
+    comments: [Comment!]
+    email: EmailAddress!
+  }
+
+  """
+  A type that describes a user's post. 
+  """
+  type Post {
+    id: ID!
+    body: String!
+    title: String!
+    author: User! 
+    isPublished: Boolean!
+    comments: [Comment!]
+  }
+
+  """
+  A type that describes a user's comment. 
+  """
+  type Comment {
+    id: ID!
+    text: String! 
+    post: Post!
+    author: User! 
+  }
+`
 
 export { typeDefs }
