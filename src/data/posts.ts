ac5538782764bee6a2bd32f195c136ff18f4eaf7
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-16 20:04:44 +0100

    Resolve posts and define field resolvers

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,3 +1,23 @@
 import { dbClient } from './config'
 
-export const getAllPosts = () => dbClient.post.findMany()
+export const getAllPosts = ({ take }: { take: number }) => (
+  dbClient.post.findMany({ take })
+)
+
+export const getUserPosts = ({
+  userId, take
+}: { userId: string, take: number }) => (
+  dbClient.user.findUnique({
+    where: {
+      id: userId
+    }
+  }).posts({ take })
+)
+
+export const getPostAuthor = ({ postId }: { postId: string }) => (
+  dbClient.post.findUnique({
+    where: {
+      id: postId
+    }
+  }).author()
+)
--- src/graphql/resolvers/Post.resolver.ts
+++ src/graphql/resolvers/Post.resolver.ts
@@ -1,11 +1,44 @@
-import { Query, Resolver } from 'type-graphql'
-import { getAllPosts } from 'data/posts'
+import { Min } from 'class-validator'
+import { Args, ArgsType, Field, FieldResolver, Int, Query, Resolver, Root } from 'type-graphql'
+import { getPostComments } from 'data/comments'
+import { getAllPosts, getPostAuthor } from 'data/posts'
+import { Comment } from 'graphql/schema/Comment.schema'
 import { Post } from 'graphql/schema/Post.schema'
+import { User } from 'graphql/schema/User.schema'
+
+@ArgsType()
+class GetPostsArgs {
+  @Field(() => Int, { nullable: true })
+  @Min(1)
+    take?: number
+}
+
+@ArgsType()
+class GetPostCommentsArgs {
+  @Field(() => Int, { nullable: true })
+  @Min(1)
+    take?: number
+}
 
 @Resolver(Post)
 export class PostResolver {
   @Query(() => [Post], { nullable: 'itemsAndList' })
-  posts () {
-    return getAllPosts()
+  posts (@Args() { take }: GetPostsArgs) {
+    return getAllPosts({ take })
+  }
+
+  @FieldResolver(() => [Comment], { nullable: 'itemsAndList' })
+  comments (
+    @Root() { id: postId }: Post,
+    @Args(() => GetPostCommentsArgs) { take }: GetPostCommentsArgs
+  ) {
+    return getPostComments({ postId, take })
+  }
+
+  @FieldResolver(() => User, { nullable: true })
+  author (
+    @Root() { id: postId }: Post
+  ) {
+    return getPostAuthor({ postId })
   }
 }
