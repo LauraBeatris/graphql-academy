1e0ebae76f847244250430d6275c34ce024adc13
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-16 20:04:15 +0100

    Resolve comments list and comment author

--- src/graphql/resolvers/Comment.resolver.ts
+++ src/graphql/resolvers/Comment.resolver.ts
@@ -1,11 +1,27 @@
-import { Query, Resolver } from 'type-graphql'
-import { getAllComments } from 'data/comments'
+import { Min } from 'class-validator'
+import { Args, ArgsType, Field, FieldResolver, Int, Query, Resolver, Root } from 'type-graphql'
+import { getAllComments, getCommentAuthor } from 'data/comments'
 import { Comment } from 'graphql/schema/Comment.schema'
+import { User } from 'graphql/schema/User.schema'
+
+@ArgsType()
+class GetCommentsArgs {
+  @Field(() => Int, { nullable: true })
+  @Min(1)
+    take?: number
+}
 
 @Resolver(Comment)
 export class CommentResolver {
   @Query(() => [Comment], { nullable: 'itemsAndList' })
-  comments () {
-    return getAllComments()
+  comments (@Args() { take }: GetCommentsArgs) {
+    return getAllComments({ take })
+  }
+
+  @FieldResolver(() => User, { nullable: true })
+  author (
+    @Root() { id: commentId }: Comment
+  ) {
+    return getCommentAuthor({ commentId })
   }
 }
