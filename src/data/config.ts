ddced90a91b727d7081a7cfedd076b25eafff19c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-17 12:24:13 +0100

    Add middleware to log the time taken for a Prims query to run

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,3 +3,18 @@ import { PrismaClient } from '@prisma/client'
 export const dbClient = new PrismaClient({
   log: ['query', 'info', 'warn', 'error']
 })
+
+/**
+ * Logs the time taken for a Prisma Query to run
+ */
+dbClient.$use(async (params, next) => {
+  const before = Date.now()
+
+  const result = await next(params)
+
+  const after = Date.now()
+
+  console.log(`⏰ Query ${params.model}.${params.action} took ${after - before}ms`)
+
+  return result
+})
