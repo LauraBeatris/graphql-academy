db2eb50907913d115be64ed36a4f0d7862381f44
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-23 15:03:48 +0100

    Move resolver logic to data layer

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,5 +1,31 @@
+import { ErrorCode } from 'graphql/schema/errors'
 import { dbClient } from './config'
 
 export const getAllUsers = ({ take }: { take: number }) => (
   dbClient.user.findMany({ take })
 )
+
+export const createUser = async ({
+  name, email
+}: { name: string; email: string; }) => {
+  const existingUser = await dbClient.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (existingUser) {
+    return {
+      code: ErrorCode.DUPLICATE_ENTRY,
+      message: "There's an existing user with the provided email.",
+      emailWasTaken: true
+    }
+  }
+
+  return dbClient.user.create({
+    data: {
+      name,
+      email
+    }
+  })
+}
--- src/graphql/resolvers/User.resolver.ts
+++ src/graphql/resolvers/User.resolver.ts
@@ -1,7 +1,7 @@
 import { Arg, Args, createUnionType, Field, FieldResolver, InputType, Mutation, ObjectType, Query, Resolver, Root } from 'type-graphql'
-import { dbClient } from 'data/config'
 import { getUserPosts } from 'data/posts'
-import { getAllUsers } from 'data/users'
+import { createUser, getAllUsers } from 'data/users'
+import { UserError } from 'graphql/schema/errors'
 import { Post } from 'graphql/schema/Post.schema'
 import { PaginationArgs } from 'graphql/schema/sharedArguments'
 import { User } from 'graphql/schema/User.schema'
@@ -11,13 +11,20 @@ class CreateUserSuccess {
   @Field(() => User)
     user: User
 }
+@ObjectType()
+class EmailTakenError {
+  @Field()
+    emailWasTaken: boolean
+}
 
 const mapMutationValueKeyToObjectType = {
-  user: CreateUserSuccess
+  user: CreateUserSuccess,
+  code: UserError,
+  emailWasTaken: EmailTakenError
 }
 const CreateUserPayload = createUnionType({
   name: 'CreateUserPayload',
-  types: () => [CreateUserSuccess] as const,
+  types: () => [CreateUserSuccess, EmailTakenError, UserError] as const,
   resolveType: mutationValue => {
     const mapperKeys = Object.keys(mapMutationValueKeyToObjectType)
     const mutationValueKey = mapperKeys.find((key) => key in mutationValue)
@@ -51,20 +58,11 @@ export class UserResolver {
   }
 
   @Mutation(() => CreateUserPayload)
-  async createUser (@Arg('data', {
+  createUser (@Arg('data', {
     description: 'Represents the input data needed to create a new user'
   }) createUserInput: CreateUserInput) {
     const { name, email } = createUserInput
 
-    const user = await dbClient.user.create({
-      data: {
-        name,
-        email
-      }
-    })
-
-    return {
-      user
-    }
+    return createUser({ name, email })
   }
 }
