8a01740bade2cc37922f34aafff16e61a6c0cc4d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-23 11:49:38 +0100

    Create base structure for createUser mutation

--- src/graphql/resolvers/User.resolver.ts
+++ src/graphql/resolvers/User.resolver.ts
@@ -1,9 +1,39 @@
 import { Min } from 'class-validator'
-import { Args, ArgsType, Field, FieldResolver, Int, Query, Resolver, Root } from 'type-graphql'
+import { Arg, Args, ArgsType, createUnionType, Field, FieldResolver, InputType, Int, Mutation, ObjectType, Query, Resolver, Root } from 'type-graphql'
+import { dbClient } from 'data/config'
 import { getUserPosts } from 'data/posts'
 import { getAllUsers } from 'data/users'
 import { Post } from 'graphql/schema/Post.schema'
 import { User } from 'graphql/schema/User.schema'
+
+@ObjectType()
+class CreateUserSuccess {
+  @Field(() => User)
+    user: User
+}
+
+const mapMutationValueKeyToObjectType = {
+  user: CreateUserSuccess
+}
+const CreateUserPayload = createUnionType({
+  name: 'CreateUserPayload',
+  types: () => [CreateUserSuccess] as const,
+  resolveType: mutationValue => {
+    const mapperKeys = Object.keys(mapMutationValueKeyToObjectType)
+    const mutationValueKey = mapperKeys.find((key) => key in mutationValue)
+
+    return mapMutationValueKeyToObjectType[mutationValueKey]
+  }
+})
+
+@InputType({ description: 'Represents the input data needed to create a user' })
+class CreateUserInput implements Partial<User> {
+  @Field()
+    name: string
+
+  @Field()
+    email: string
+}
 
 @ArgsType()
 class GetUsersArgs {
@@ -24,6 +54,22 @@ export class UserResolver {
   @Query(() => [User], { nullable: 'itemsAndList' })
   users (@Args() { take }: GetUsersArgs) {
     return getAllUsers({ take })
+  }
+
+  @Mutation(() => CreateUserPayload)
+  async createUser (@Arg('data') createUserInput: CreateUserInput) {
+    const { name, email } = createUserInput
+
+    const user = dbClient.user.create({
+      data: {
+        name,
+        email
+      }
+    })
+
+    return {
+      user
+    }
   }
 
   @FieldResolver(() => [Post], { nullable: 'itemsAndList' })
