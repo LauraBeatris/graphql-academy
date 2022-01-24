db5303659a6b1c6f031a150404a0d84ff02f6c66
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-24 16:05:45 +0100

    Add migration for referencial actions

---
+++ prisma/migrations/20220124150243_add_cascade_actions/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_authorId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
