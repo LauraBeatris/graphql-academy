c0505f9d99f4779773b582f81c33a270941768e6
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-01-25 14:34:28 +0100

    Add mutations to create/delete comments

--- src/data/comments.ts
+++ src/data/comments.ts
@@ -1,3 +1,6 @@
+import { CreateCommentSuccess, DeleteCommentSuccess } from 'graphql/resolvers/comment'
+import { ErrorCode } from 'graphql/schema/enums/errorCode'
+import { UserError } from 'graphql/schema/types/userError'
 import { dbClient } from './config'
 
 export const getAllComments = ({ take }: { take: number }) => (
@@ -21,3 +24,65 @@ export const getCommentAuthor = ({ commentId }: { commentId: string }) => (
     }
   }).author()
 )
+
+export const createComment = async ({ text, postId, authorId }: {
+  text: string;
+  postId: string;
+  authorId: string;
+}) => {
+  const post = await dbClient.post.findUnique({
+    where: {
+      id: postId
+    }
+  })
+  if (!post) {
+    return Object.assign(new UserError(), {
+      code: ErrorCode.NOT_FOUND,
+      path: ['comment', 'post', 'id'],
+      message: 'Not able to find a post with the postId provided.'
+    })
+  }
+
+  const author = await dbClient.user.findUnique({
+    where: {
+      id: authorId
+    }
+  })
+  if (!author) {
+    return Object.assign(new UserError(), {
+      code: ErrorCode.NOT_FOUND,
+      path: ['comment', 'user', 'id'],
+      message: 'Not able to find an user with the authorId provided.'
+    })
+  }
+
+  return Object.assign(new CreateCommentSuccess(), {
+    comment: await dbClient.comment.create({
+      data: {
+        text,
+        postId,
+        authorId
+      }
+    })
+  })
+}
+
+export const deleteComment = async ({ id }: { id: string }) => {
+  const comment = await dbClient.comment.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if (!comment) {
+    return Object.assign(new UserError(), {
+      code: ErrorCode.NOT_FOUND,
+      path: ['comment', 'id'],
+      message: 'Comment not found'
+    })
+  }
+
+  return Object.assign(new DeleteCommentSuccess(), {
+    comment: await dbClient.comment.delete({ where: { id } })
+  })
+}
--- src/graphql/resolvers/comment.ts
+++ src/graphql/resolvers/comment.ts
@@ -1,8 +1,45 @@
-import { Args, FieldResolver, Query, Resolver, Root } from 'type-graphql'
-import { getAllComments, getCommentAuthor } from 'data/comments'
+import { Arg, Args, createUnionType, Field, FieldResolver, ID, InputType, Mutation, ObjectType, Query, Resolver, Root } from 'type-graphql'
+import { createComment, deleteComment, getAllComments, getCommentAuthor } from 'data/comments'
 import { PaginationArgs } from 'graphql/schema/arguments/pagination'
 import { Comment } from 'graphql/schema/types/comment'
 import { User } from 'graphql/schema/types/user'
+import { UserError } from 'graphql/schema/types/userError'
+
+@InputType()
+class CreateCommentInput implements Partial<Comment> {
+  @Field()
+    text: string
+
+  @Field(() => ID)
+    postId: string
+
+  @Field(() => ID)
+    authorId: string
+}
+@ObjectType()
+export class CreateCommentSuccess {
+  @Field(() => Comment)
+    comment: Comment
+}
+const CreateCommentPayload = createUnionType({
+  name: 'CreateCommentPayload',
+  types: () => [CreateCommentSuccess, UserError] as const
+})
+
+@InputType()
+class DeleteCommentInput implements Partial<Comment> {
+  @Field(() => ID)
+    id: string
+}
+@ObjectType()
+export class DeleteCommentSuccess {
+  @Field(() => Comment)
+    comment: Comment
+}
+const DeleteCommentPayload = createUnionType({
+  name: 'DeleteCommentPayload',
+  types: () => [DeleteCommentSuccess, UserError] as const
+})
 
 @Resolver(Comment)
 export class CommentResolver {
@@ -16,5 +53,15 @@ export class CommentResolver {
     @Root() { id: commentId }: Comment
   ) {
     return getCommentAuthor({ commentId })
+  }
+
+  @Mutation(() => CreateCommentPayload)
+  createComment (@Arg('data') { text, postId, authorId }: CreateCommentInput) {
+    return createComment({ text, postId, authorId })
+  }
+
+  @Mutation(() => DeleteCommentPayload)
+  deleteComment (@Arg('data') { id }: DeleteCommentInput) {
+    return deleteComment({ id })
   }
 }
