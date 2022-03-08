dd55e8e43eca281913a296faddc2ca455a59d8c8
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-03-08 21:26:19 +0100

    Emit SDL artifact

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server'
 import { buildSchemaSync } from 'type-graphql'
+import { resolve } from 'path'
 
 import { CommentResolver, MarketingPostResolver, PostResolver, UserResolver } from './graphql/resolvers'
 
 const server = new ApolloServer({
   schema: buildSchemaSync({
+    emitSchemaFile: resolve(__dirname, '__generated__/graphql/schema.gql'),
     resolvers: [UserResolver, CommentResolver, PostResolver, MarketingPostResolver]
   })
 })
